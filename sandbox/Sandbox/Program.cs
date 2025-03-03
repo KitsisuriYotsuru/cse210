a3a885f976e9fe4d7e861b026e614eb2be31cfb5
KitsisuriYotsuru <olliverroccs@gmail.com>
2025-03-03 10:01:37 -0700

    making classes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,47 @@
 using System;
 
+class Circle
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double GetArea()
+    {
+        double area = Math.PI * radius * radius;
+        return area;
+    }
+
+    public void DisplayCircleArea()
+    {
+       Console.WriteLine($"The area of the circle with radius {radius} is: {GetArea()}");
+    }
+
+    public void SetRadius(double radius)
+    {
+        this.radius = radius;
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello Today - March 3, 2025");
+
+        // double x = 10;
+        Circle myCircle = new Circle(10);
+        myCircle.DisplayCircleArea();
+        Circle myCircle2 = new Circle(20);
+        myCircle2.DisplayCircleArea();
+
+        for(int i = 0; i < 30; i++)
+        {
+            myCircle.SetRadius(i);
+            myCircle.DisplayCircleArea();
+        }
     }
 }
